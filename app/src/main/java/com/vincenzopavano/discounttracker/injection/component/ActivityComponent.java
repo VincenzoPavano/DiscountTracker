33b37e187586f41bccdb2ab8f62fad19248b10c2
Vincenzo Pavano <vincenzo@raccordollc.com>
2018-11-22 23:43:54 -0500

    Removed boilerplate code

--- app/src/main/java/com/vincenzopavano/discounttracker/injection/component/ActivityComponent.java
+++ app/src/main/java/com/vincenzopavano/discounttracker/injection/component/ActivityComponent.java
@@ -1,7 +1,7 @@
 package com.vincenzopavano.discounttracker.injection.component;
 
 import dagger.Subcomponent;
-import com.vincenzopavano.discounttracker.features.detail.DetailActivity;
+
 import com.vincenzopavano.discounttracker.features.main.MainActivity;
 import com.vincenzopavano.discounttracker.injection.PerActivity;
 import com.vincenzopavano.discounttracker.injection.module.ActivityModule;
@@ -11,6 +11,4 @@ import com.vincenzopavano.discounttracker.injection.module.ActivityModule;
 public interface ActivityComponent {
 
     void inject(MainActivity mainActivity);
-
-    void inject(DetailActivity detailActivity);
 }
